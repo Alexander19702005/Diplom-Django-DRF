0fde393dadb234a8dbc9a9a9bae30cfe00f8def5
Alexander19702005 <asapogov@mail.ru>
2025-03-19 13:19:04 +0500

    Update views.py

--- spd-diplom-main/social_network/posts/views.py
+++ spd-diplom-main/social_network/posts/views.py
@@ -1,4 +1,3 @@
-
 from rest_framework.decorators import api_view, permission_classes
 from .models import Post
 from .models import Post_1
@@ -8,57 +7,57 @@ from .serializers import PostSerializer
 from .serializers import Post_1Serializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.permissions import (IsAuthenticated, IsAdminUser)
+from rest_framework.permissions import (IsAuthenticated, IsAdminUser, SAFE_METHODS)
 from rest_framework.permissions import   (IsAuthenticatedOrReadOnly)
 from .permissions import (IsOwnerOrReadOnly)
-
+from rest_framework import generics
 
 
 class PostView(APIView):
-
+    permission_classes = ([IsOwnerOrReadOnly])
     def get (self,request, *arqs , **kwargs):
-        permission_classes([IsAuthenticated])
         queryset=Post.objects.all()
         serializer=PostSerializer(queryset,many=True)
         return Response(serializer.data)
 
 
     def post(self,request,*args,**kwargs):
-        permission_classes(IsOwnerOrReadOnly)
         queryset = Post.objects.all()
         serializer = PostSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
         return Response(serializer.data)
-    def patch(self,request,*args,**kwargs):
-        permission_classes(IsOwnerOrReadOnly)
+
+
+    def patch(self,request,*args,**kwargs):        
         queryset = Post.objects.all()
         serializer = PostSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
         return Response(serializer.data)
-    def delete(self,request,*args,**kwargs):
-        permission_classes([IsAdminUser])
+
+
+    def delete(self,request,*args,**kwargs):       
+
         queryset = Post.objects.all()
         serializer = PostSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
         return Response(serializer.data)
+
 class Post_1View(APIView):
+    permission_classes([IsOwnerOrReadOnly])
     def get (self,request, *arqs , **kwargs):
-        permission_classes([IsOwnerOrReadOnly])
         queryset=Post_1.objects.all()
         serializer=Post_1Serializer(queryset,many=True)
         return Response(serializer.data)
     def post (self,request, *arqs , **kwargs):
-        permission_classes(IsAuthenticated)
         queryset = Post_1.objects.all()
         serializer = Post_1Serializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
         return Response(serializer.data)
     def patch (self,request, *arqs , **kwargs):
-        permission_classes([IsOwnerOrReadOnly])
         queryset = Post_1.objects.all()
         serializer = Post_1Serializer(data=request.data)
         if serializer.is_valid():
@@ -67,19 +66,18 @@ class Post_1View(APIView):
 
 
 class LikeView(APIView):
-
+    permission_classes=(IsAuthenticated)
     def get(self, request, *arqs, **kwargs):
-        permission_classes(IsAuthenticated)
         queryset = Like.objects.all()
         serializer = LikeSerializer(queryset, many=True)
         return Response(serializer.data)
 
     def post(self, request, *arqs, **kwargs):
-        permission_classes(IsAuthenticated)
         queryset = Like.objects.all()
         serializer = LikeSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
         return Response(serializer.data)
+
 
 
