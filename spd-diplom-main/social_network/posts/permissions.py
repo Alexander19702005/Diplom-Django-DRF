b61bb7d7a532194b14846dfadcbc39b5c4feaefd
Alexander19702005 <asapogov@mail.ru>
2025-04-07 09:13:05 +0500

    Update permissions.py

--- spd-diplom-main/social_network/posts/permissions.py
+++ spd-diplom-main/social_network/posts/permissions.py
@@ -1,12 +1,12 @@
 from rest_framework import permissions
-from rest_framework.permissions import BasePermission, IsAuthenticated, SAFE_METHODS
-from rest_framework.response import Response
-from rest_framework.views import APIView
-from rest_framework import permissions
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
-from rest_framework.views import APIView
 
+class IsOwnerOrReadOnly(permissions.BasePermission):
+
+    def has_object_permission(self, request, view, obj):
+       if request.method in permissions.SAFE_METHODS:
+            return True
+
+       return obj.user == request.user
 
 
 
@@ -24,20 +24,3 @@ class AuthorOrReadOnly(permissions.BasePermission):
         return False
 
 
-
-# class ExampleView(APIView):
-#     permission_classes = [IsAuthenticated]
-#
-#     def get(self, request, format=None):
-#         content = {
-#             'status': 'request was permitted'
-# #         }
-
-
-# class IsOwnerOrReadOnly(permissions.BasePermission):
-
-#     def has_object_permission(self, request, view, obj):
-#        if request.method in permissions.SAFE_METHODS:
-#             return True
-
-#        return obj.user == request.user
