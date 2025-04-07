c74ea256021911c9feb33f8b93f8becb7b0a244b
Alexander19702005 <asapogov@mail.ru>
2025-04-07 09:06:38 +0500

    Update serializers.py

--- spd-diplom-main/social_network/posts/serializers.py
+++ spd-diplom-main/social_network/posts/serializers.py
@@ -1,7 +1,5 @@
 from rest_framework import serializers
-from .models import Post
-from .models import Post_1
-from .models import Like
+from .models import Post,Comment,Like
 from django.db.models import Count,Max,Min,Avg
 
 
@@ -9,25 +7,25 @@ from django.db.models import Count,Max,Min,Avg
 
  #  Сериализатор    для    комментариев
 
-class Post_1Serializer(serializers.ModelSerializer):
+class CommentSerializer(serializers.ModelSerializer):
 
     class Meta:
-        model = Post_1
+        model = Comment
         fields ='__all__'
 
 
 # Сериализатор для постов с фото
 class PostSerializer(serializers.ModelSerializer):
     author=serializers.HiddenField(default=serializers.CurrentUserDefault())
-    comment=Post_1Serializer(many=False,read_only=True,source=('author'))
+    comment=CommentSerializer(many=False,read_only=True,source=('author'))
     likes_count = serializers.SerializerMethodField()
 
     def get_likes_count(self, obj):
-        return Like.objects.aggregate(likes_count=Count('like'))
+         return Like.objects.filter(post=obj).count()
 
     class Meta:
         model=Post
-        fields=['id','text','author','created_at','image','comment','likes_count']
+        fields=['id','text','author','created_at','image','likes_count','comment']
 #Сериализатор для лайков
 class LikeSerializer(serializers.ModelSerializer):
     class Meta:
